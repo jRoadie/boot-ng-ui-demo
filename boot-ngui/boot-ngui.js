257ced6559d2b3d76ebacadbd204c42bbe1a8a2c
Me <xenon.mbstu@gmail.com>
2014-12-19 01:28:59 +0600

    grid: (row and column) implemented

--- boot-ngui/boot-ngui.js
+++ boot-ngui/boot-ngui.js
@@ -1,6 +1,55 @@
 (function() {
     var bng = angular.module('boot-ngui', []);
     var directives = bng.directives = {};
+    directives.Row = function() {
+        return {
+            restrict: 'E',
+            transclude: true,
+            templateUrl: utils.getTemplate('row'),
+            link: function(scope, el, attrs) {
+                scope.id = attrs.id;
+                scope.class = attrs.class;
+            }
+        }
+    };
+    directives.Column = function() {
+        return {
+            restrict: 'E',
+            transclude: true,
+            templateUrl: utils.getTemplate('column'),
+            link: function(scope, el, attrs) {
+                var prepareClasses = function(attr, resolver) {
+                    return attr.split(' ').map(resolver).join(' ');
+                };
+                var clazz = "";
+                if(attrs.size) {
+                    clazz += prepareClasses(attrs.size, function(v) {
+                        return 'col-' + v;
+                    })
+                }
+                if(attrs.offset) {
+                    clazz += ' ' + prepareClasses(attrs.offset, function(v) {
+                        var idx = v.lastIndexOf('-');
+                        return 'col-' + v.substr(0, idx+1) + 'offset-' + v[v.length-1];
+                    })
+                }
+                if(attrs.push) {
+                    clazz += ' ' + prepareClasses(attrs.push, function(v) {
+                        var idx = v.lastIndexOf('-');
+                        return 'col-' + v.substr(0, idx+1) + 'push-' + v[v.length-1];
+                    })
+                }
+                if(attrs.pull) {
+                    clazz += ' ' + prepareClasses(attrs.pull, function(v) {
+                        var idx = v.lastIndexOf('-');
+                        return 'col-' + v.substr(0, idx+1) + 'pull-' + v[v.length-1];
+                    })
+                }
+                scope.id = attrs.id;
+                scope.class = clazz;
+            }
+        }
+    };
     directives.GlyphIcon = function() {
         return {
             restrict: 'E',
@@ -35,7 +84,7 @@
             transclude: true,
             templateUrl: utils.getTemplate('well'),
             link: function(scope, el, attrs) {
-                scope.size = attrs.size === 'small' ? 'sm' : 'lg';
+                scope.size = attrs.size;
             }
         }
     };
@@ -58,6 +107,10 @@
             }
         }
     };
+    bng.directive('row', directives.Row);
+    bng.directive('bngRow', directives.Row);
+    bng.directive('bngCol', directives.Column);
+    bng.directive('kol', directives.Column);
     bng.directive('glyphicon', directives.GlyphIcon);
     bng.directive('alert', directives.Alert);
     bng.directive('bngAlert', directives.Alert);
